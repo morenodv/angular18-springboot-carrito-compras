3686f83a9ba875a5d93948546d65b255a47155f1
Jesus <jesuscta08@gmail.com>
2024-12-27 23:28:39 -0600

    App component modificado, nuevo componente carApp creado - eliminado archivo spec y css

---
+++ src/app/components/car-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'cart-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-app.component.html'
+})
+export class CarAppComponent {
+
+}
