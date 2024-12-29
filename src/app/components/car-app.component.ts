aab29518bd7a571375a50162746fe51f54c10d38
Jesus <jesuscta08@gmail.com>
2024-12-29 16:40:40 -0600

    Proyecto separado por componentes, funcionamiento del carrito de compras (agregar y evitar duplicados)

--- src/app/components/car-app.component.ts
+++ src/app/components/car-app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent, ProductCardComponent, CartComponent],
   templateUrl: './car-app.component.html'
 })
-export class CarAppComponent {
+export class CarAppComponent implements OnInit {
 
+
+  products: Product[] = [];
+
+  items: CartItem[] = [];
+
+  constructor(private service: ProductService){
+
+  }
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+    }
+
+    onAddCart(product: Product){
+      const hasItem = this.items.find( item => item.product.id === product.id);
+      if(hasItem){
+        this.items = this.items.map(item =>{
+          if(item.product.id === product.id) {
+            return {
+              ... item,
+              quantity: item.quantity + 1
+            }
+          }
+          return item;
+        })
+      } else {
+        this.items = [... this.items, { product : { ... product }, quantity: 1 }]
+      }
+    }
 }
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,41 @@
+import { Product } from "../models/product";
+
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Teclado mecanico Asus',
+    description: 'teclado mecanico 100',
+    price: 1000
+  },
+  {
+    id: 2,
+    name: 'Samsung Smart TV',
+    description: 'pantalla 75 in oled',
+    price: 2000
+  },
+  {
+    id: 3,
+    name: 'Camara sony video',
+    description: 'Camara de video 1080',
+    price: 3000
+  },
+  {
+    id: 4,
+    name: 'Corsair memoria ram ddr5',
+    description: '16 gb ram memoria',
+    price: 4000
+  },
+  {
+    id: 5,
+    name: 'Nvidia 4090',
+    description: 'tarjeta de video para juegos 16 gb vram',
+    price: 5000
+  },
+  {
+    id: 6,
+    name: 'Intel Core i7',
+    description: 'CPU para pc',
+    price: 6000
+  },
+]
